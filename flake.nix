d3bc820e6b4af3d0d3488f3049c8288110fc36f1
yutakobaayshidev <hi@yutakobayashi.dev>
2025-02-15 05:35:19 +0900

    feat(flake): curl,git

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  description = "A very basic flake";
+  description = "Minimal package definition for aarch64-darwin";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
-
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
-
+    packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
+      name = "my-packages-list";
+      paths = [
+        nixpkgs.legacyPackages.aarch64-darwin.git
+        nixpkgs.legacyPackages.aarch64-darwin.curl
+      ];
+    };
   };
 }
